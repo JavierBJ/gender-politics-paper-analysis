f76eadb12508987c4ea0c62525a8252a389374ff
javierbj <javierbeltranj@gmail.com>
2018-12-14 13:38:25 +0100

    Documentation + changes in command line args

--- analyze.py
+++ analyze.py
@@ -1,3 +1,25 @@
+"""gender-politics-paper-analysis: analyze.py
+
+This module contains the main code for the language analysis by gender, as well as the functions that run the machine
+learning experiments. It can be called from the command line using the following arguments:
+
+python3 analyze.py path alg
+    where <path> is the path to a CSV file containing two columns called "tweet" and "gender" in the header, and
+    separated by <;>,
+    and <alg> can be one of the following: lasso, l1, ridge, l2, svm. l1 is a shortcut for lasso, and l2 is a shortcut
+    for ridge.
+    Optional arguments are also available. For a complete description of the program please type python3 analyze.py -h
+
+The code makes heavy use of well-known libraries from the ecosystem of scientific computing and data science in Python,
+like numpy, pandas and scikit-learn. It also calls the module text.py, which contains the pre-processing steps using
+the libraries FreeLing 4.0 and NLTK.
+
+Author: Javier Beltran Jorba
+Institut Barcelona d'Estudis Internacionals
+Universitat Pompeu Fabra
+"""
+
+
 import text
 import os, argparse
 import numpy as np
@@ -9,9 +31,17 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
 
 
-def results_to_file(results, names, to, top=50):
+def results_to_file(coefficients, names, to, top):
+    """Prints the list of most predictive words for males and females in a file.
+
+    :param coefficients: 1D array of coefficients of the best model found.
+    :param names: list with the words corresponding to each coefficient, index-wise.
+    :param to: path of the output file.
+    :param top: number of most predictive words to print. The same number is printed for males and females.
+    :return: nothing. Prints results to the filesystem.
+    """
     to = open(to, 'w')
-    l = [(names[x], v) for (x, v) in enumerate(results)]
+    l = [(names[x], v) for (x, v) in enumerate(coefficients)]
     males = sorted(l, key=lambda x: x[1], reverse=True)[:top]
     females = sorted(l, key=lambda x: x[1])[:top]
     print('Male predictors:', file=to)
@@ -25,68 +55,99 @@ def results_to_file(results, names, to, top=50):
 
 
 def train_and_evaluate(X, y, X_test, y_test, model, folds):
+    """Trains a model with the given dataset, using k-fold cross validation and saving the scores (balanced accuracy) for
+    training, validation and test. Test score is calculated here for commodity even though it is still unknown by
+    the analyzer if this model configuration will be the best. It is not used for anything but being ready for printing
+    once all models are checked.
+
+    :param X: training dataset of tweets already preprocessed and converted into feature counts, i.e. a binary matrix
+    of (n_tweets, n_features)
+    :param y: 1D array of labels for the tweets in X (1 for males, -1 for females but any two values should work).
+    :param X_test: test dataset.
+    :param y_test: test labels.
+    :param model: a sklearn supervised algorithm with fit(X) and predict(X) methods.
+    :param folds: number of folds in the k-fold cross validation.
+    :return: three data, 1) 1D array with the coefficients of the model fitted; 2) a dictionary with the training,
+    validation and test scores accessible by keys 'Training', 'Validation' and 'Test' respectively; 3) confusion matrix.
+    """
+
+    # Divides training dataset into training and validation, using a different split for validation at each iteration.
     kf = KFold(n_splits=folds)
-    results = np.array((X.get_shape()[1],))
     train_score = 0
     val_score = 0
     for train_idx, val_idx in kf.split(X):
+        # Fit model with training part
         model = model.fit(X[train_idx], y[train_idx])
+
+        # Predict with two datasets and get balanced accuracy score
         tr_preds = model.predict(X[train_idx])
         val_preds = model.predict(X[val_idx])
         train_score += metrics.roc_auc_score(y[train_idx], tr_preds)
         val_score += metrics.roc_auc_score(y[val_idx], val_preds)
-        results = np.add(results, model.coef_.flatten(), casting='unsafe')
-    results /= folds
+
+    # Average scores for the k folds.
     train_score /= folds
     val_score /= folds
 
     # Re-train with the whole training set
     model = model.fit(X, y)
-    results = model.coef_.flatten()
+    coefficients = model.coef_.flatten()
 
     # Let test scores calculated, though they must not be observed during fine-tuning
     te_preds = model.predict(X_test)
     test_score = metrics.roc_auc_score(y_test, te_preds)
 
+    # Save evaluation measures: scores and confusion matrix
     conf = metrics.confusion_matrix(y_test, te_preds)
     scores = {'Training':train_score, 'Validation':val_score, 'Test':test_score}
-    return results, scores, conf
+    return coefficients, scores, conf
 
 
-def analyze(path, alg='lasso', alpha=1, kfolds=10, kwr=5000, sw=True):
-    # Craft path for outputs from parameters
-    out_path = 'out/'
+def analyze(path, alg, alpha, kfolds, rank, top):
+    """Performs the machine learning experiments with the dataset, algorithm, and parameters selected; and writes the
+    results in different files inside an out/ folder: a log file with the scores obtained by each configuration of the
+    algorithm, and a file with the most predictive words for each configuration.
+
+    :param path: path to the dataset in csv format, using ; as separator and with two columns, tweet and gender. A
+    header should be present in the first line with these column names.
+    :param alg: string with the name of the algorithm to be used (lasso (or l1), ridge (or l2), svm).
+    :param alpha: regularization parameter of the learning algorithm (higher means more regularization).
+    :param kfolds: k parameter of k-fold cross validation (in how many chunks to divide the dataset and how many
+    repetitions to perform).
+    :param rank: only the most frequent words will be used as features. This parameter chooses how many.
+    :param top: only the most predictive words will be printed to a file. This parameter chooses how many.
+    :return: nothing. Prints results to the filesystem.
+    """
+
+    # Create paths for writing results
+    dir_path = 'out/'
     expname = path.split('/')[-1].split('.')[0] + '_' + alg
-    if kwr is not None:
-        expname += '_rank' + str(kwr)
-    if sw:
-        expname += '_sw'
-    out_path += expname
-    if not os.path.exists(out_path):
-        os.makedirs(out_path)
-    log = open(out_path + '/log.txt', 'w')
+    dir_path += expname
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+    log = open(dir_path + '/log.txt', 'w')
 
     # Load CSV from path
     tweets_df = pd.read_csv(path, delimiter=';', encoding='utf-8')
     label = tweets_df['gender']
     tweets_df = tweets_df['tweet']
 
-    # Pre-process and feature extraction
-    tweets_df, label = text.preprocess(tweets_df, label)
+    # Pre-process tweets and divide into training and test
+    tweets_df = text.preprocess(tweets_df)
     y = np.array(label).reshape((-1,))
     x_train, x_test, y_train, y_test = train_test_split(tweets_df, y, test_size=0.2)
 
     # Format required by default CountVectorizer is sequence of strings separated by e.g. spaces
     x_train = [' '.join(x) for x in x_train]
     x_test = [' '.join(x) for x in x_test]
-    feat_ext = CountVectorizer(encoding='utf-8', token_pattern=' ', ngram_range=(1,1), analyzer='word',
-                               max_features=kwr, binary=True)
+    feat_ext = CountVectorizer(encoding='utf-8', ngram_range=(1,1), analyzer='word', max_features=rank, binary=True)
     x_train = feat_ext.fit_transform(x_train)
     x_test = feat_ext.transform(x_test)
 
     best_val = 0
     best_test = 0
     for a in alpha:
+        # Choose learning algorithm based on arguments
         c_reg = 1 / a
         if alg in ['lasso', 'l1']:
             model = LogisticRegression(class_weight='balanced', solver='liblinear', penalty='l1', C=c_reg)
@@ -95,39 +156,44 @@ def analyze(path, alg='lasso', alpha=1, kfolds=10, kwr=5000, sw=True):
         elif alg == 'svm':
             model = LinearSVC(class_weight='balanced', C=c_reg)
 
-        # Run experiments
+        # Fit model and test it
         results, scores, confusion = train_and_evaluate(x_train, y_train, x_test, y_test, model, kfolds)
 
         # Write to results file
-        os.makedirs(out_path, exist_ok=True)
-        out_path += '/' + 'a' + str(a).replace('.', '_') + '.txt'
-        results_to_file(results=results, names=feat_ext.get_feature_names(), to=out_path, top=500)
+        out_path = dir_path + '/' + 'a' + str(a).replace('.', '_') + '.txt'
+        results_to_file(coefficients=results, names=feat_ext.get_feature_names(), to=out_path, top=top)
 
         # Write to log file
         print('Experiment', expname, ', case', a, '...', file=log)
-        print('Training score:', scores['Training'])
-        print('Validation score:', scores['Validation'])
+        print('Training score:', scores['Training'], file=log)
+        print('Validation score:', scores['Validation'], file=log)
         print('', file=log)
 
+        # Save best configuration of algorithm to present test score later
         if scores['Validation'] > best_val:
             best_val = scores['Validation']
             best_test = scores['Test']
             best_a = a
 
+    # Write test score to log file
     print('Best experiment: Alpha =', best_a, file=log)
     print('Test score:', best_test, file=log)
 
 
 if __name__ == '__main__':
+    """Main code of the analyzer of language by gender. Reads execution arguments or informs the user if the arguments 
+    are wrong.
+    """
     parser = argparse.ArgumentParser(description='run experiments of language analysis.')
     parser.add_argument('path',
                         help='path to csv file with dataset')
     parser.add_argument('alg', choices=['lasso', 'l1', 'ridge', 'l2', 'svm'], help='learning algorithm')
-    parser.add_argument('-a', '--alpha', type=float, nargs='+', default=1.0, help='regularization parameters to try')
+    parser.add_argument('-a', '--alpha', type=float, nargs='+', default=[1.0], help='regularization parameters to try')
     parser.add_argument('-k', '--kfolds', type=int, default=10, help='k for k-fold cross-validation')
-    parser.add_argument('-r', '--keepwordsrank', type=int,
+    parser.add_argument('-r', '--rankfeatures', type=int, default=5000,
                         help='number of features to be used ranked by number of appearances')
-    parser.add_argument('-s', '--stopwords', action='store_true', help='remove stopwords if active')
+    parser.add_argument('-t', '--topfeatures', type=int, default=50,
+                        help='number of word features to be printed in the output files')
     args = parser.parse_args()
 
-    analyze(args.path, args.alg, args.alpha, args.kfolds, args.keepwordsrank, args.stopwords)
+    analyze(args.path, args.alg, args.alpha, args.kfolds, args.rankfeatures, args.topfeatures)
--- text.py
+++ text.py
@@ -1,9 +1,31 @@
+"""gender-politics-paper-analysis: text.py
+
+This module is in charge of the pre-processing of tweets. It uses FreeLing 4.0, NLTK and an emoji library for that task,
+so it is basically a handler for these tools. It also contains hand-crafted filters that keep some tokens out of the
+dataset.
+
+If you want to use another pre-processing strategy or just other libraries, you can ignore this module and change
+the call to preprocess() in the analyze.py module for your own code.
+
+Author: Javier Beltran Jorba
+Institut Barcelona d'Estudis Internacionals
+Universitat Pompeu Fabra
+"""
+
+
 import pyfreeling
 import configparser
 from nltk.corpus import stopwords
 
 
 def setup_freeling():
+    """
+    Loads FreeLing with the settings for Spanish. It's called inside preprocess(). Paths with the Spanish settings
+    are read from the file config.ini. If your installation of FreeLing differs from the typical, chances are that
+    you must change the paths.
+
+    :return: 4 FreeLing components for pre-processing
+    """
     config = configparser.ConfigParser()
     config.read('config.ini')
     pyfreeling.util_init_locale('default')
@@ -30,46 +52,77 @@ def setup_freeling():
 
 
 def is_proper(t):
+    """
+    Filter for proper nouns in the tokens
+    :param t: the POS tag of the token
+    :return: True if FreeLing tagged the token as Noun and Proper, False otherwise
+    """
     return t[0:2]=='NP'
 
 
 def is_sw(w):
+    """
+    Filter for stopwords in the tokens
+
+    :param w: the word of the token
+    :return: True if the word is in the list of stopwords from NLTK, False otherwise
+    """
     return w.lower() in stopwords.words('spanish')
 
 
 def is_punct(w):
+    """
+    Filter for tokens that are basically punctuation or special symbols
+
+    :param w: the word of the token
+    :return: True if the word is punctuation, False otherwise
+    """
     from string import punctuation
     for p in punctuation:
         w = w.replace(p, '')
     for n in '0123456789':
         w = w.replace(n, '')
+    # If, after removing all special symbols and numbers, the remaining is 0-3 characters long, then it's filtered.
     return is_short(w)
 
 
 def is_short(w):
+    """
+    Filter for short tokens
+
+    :param w: the word of the token
+    :return: True if the word is short, False otherwise
+    """
     import emoji
     if w.startswith('@') or w.startswith('#') or w.startswith('https://'):
+        # Mentions, Hashtags and URLs are also considered short words
         return True
     elif len(w) == 1:
+        # Emojis are NOT considered short words
         return w[0] not in emoji.UNICODE_EMOJI
     else:
+        # Words shorter than 3 words are considered short
         return len(w) < 3
 
 
-def preprocess(tweets, label):
+def preprocess(tweets):
+    """
+    Pre-process the dataset: tokenizes, lemmatizes, and analyzes it morphologically to determine its POS tags.
+    :param tweets: the dataset as a list of strings (the tweets)
+    :return: the dataset already processed i.e. the tweets as tokens
+    """
     lemmas = []
-    labels = []
     tk, sp, umap, mf = setup_freeling()
-    for tw, l in zip(tweets, label):
+    for tw in tweets:
         tokens = tk.tokenize(tw)
         tokens = sp.split(tokens)
         tokens = umap.analyze(tokens)
         tokens = mf.analyze(tokens)
 
+        # Only keep those tokens that pass certain filters
         lemmas.append([x.get_lemma() for sent in tokens for x in sent
                        if not is_proper(x.get_tag())
                        and not is_sw(x.get_lemma())
                        and not is_punct(x.get_lemma())
                        and not is_short(x.get_lemma())])
-        labels.append(l)
-    return lemmas, labels
+    return lemmas
